31794b7d3801aed69ac6755db3ba8136688836a8
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-08-17 08:48:20 +0530

    Create Pattern-5: Inverted Right Pyramid.java

---
+++ Patterns/Pattern-5: Inverted Right Pyramid.java
@@ -0,0 +1,14 @@
+public class Solution
+  {
+    public static void seeding(int n) 
+    {
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=i;j<=n;j++)
+           {
+               System.out.print(j+" ");
+           }
+           System.out.println();
+       } 
+    }
+}
