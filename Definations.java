927e512247f09b16a403310df637614ab8ba2827
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-08-21 18:57:19 +0530

    Create Definations.java
    Definations  What  I learn day by day

---
+++ Definations.java
@@ -0,0 +1,5 @@
+1. What is Data Structure?
+   A data structure is defined as a particular way of storing and organizing
+   data in our devices to use the data efficiently and effectively. The main
+   idea behind using data structures is to minimize the time and space complexities.
+   An efficient data structure takes minimum memory space and requires minimum time to execute the data.
