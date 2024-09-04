106a9d8d397340ae63d839b1f158ab2bf1fd9737
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-09-04 21:41:43 +0530

    Create PrintNumber(1 to N).java

---
+++ Recursions.java/PrintNumber(1 to N).java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Recursion{
+    public static void PrintNumber(int i, int num){
+        if(i>num){
+            return;
+        }
+        System.out.println(i);
+        PrintNumber(i+1,num);
+        
+    }
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int num= sc.nextInt();
+        PrintNumber(1,num);
+    }
+}
+
