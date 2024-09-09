0f669f645a8505171a0e81e0c13b94b20f6191e2
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-09-09 22:39:36 +0530

    Update stackArray.java

--- stacks/stackArray.java
+++ stacks/stackArray.java
@@ -1,1 +1,89 @@
-
+import java.util.*;
+public class stack {
+    int size ;
+    int top;
+    int[] number ;
+    public stack(int size){
+        this.size=size;
+        top=-1;
+        number= new int[size];
+    }
+    public boolean isFull(){
+        if(top==size-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean isEmpty(){
+        if(top==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void push(int data){
+        if(isFull()){
+            System.out.println("Stack is Full");
+        }
+        else{
+           top++;
+         number[top]=data;
+           System.out.println("Push data  " +data);
+        }
+    }
+    public int  pop(){
+        int del_data=0;
+        if(isEmpty()){
+            System.out.println("stack is Empty");
+        }
+        else{
+        
+           del_data = number[top];
+           top--;
+           System.out.println("Deleted data"+del_data);
+        }
+        return del_data;
+    }
+    public void display(){
+        if(top==-1){
+            System.out.println("Stack is Over flow");
+        }
+        else{
+             for (int i = 0; i <= top; i++) {
+                System.out.println(number[i]);
+            }
+        }
+    }
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the size");
+       int size=sc.nextInt();
+        stack st= new stack(size);
+        int choice=0;
+     
+                    
+        do{
+            System.out.println("Enter  choice 1. push 2. pop 3. disply");
+            choice=sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the data");
+                    int data = sc.nextInt();
+                    st.push(data);
+                    break;
+                case 2:
+                   System.out.println(st.pop());
+                   break;
+                 case 3:
+                   st.display();
+                   break;
+                 case 4:
+                     default:
+                     System.out.println("Sorry");
+            }
+        }while(choice!=3);    
+    }
+}
