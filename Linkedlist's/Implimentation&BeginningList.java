7f896a893d67c096ccc2fcd09b5c038ef956f835
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-09-22 18:23:19 +0530

    Update Implimentation&BeginningList.java

--- Linkedlist's/Implimentation&BeginningList.java
+++ Linkedlist's/Implimentation&BeginningList.java
@@ -1,74 +1,67 @@
 import java.util.*;
 class Node{
      int data;
-     
      Node link;
 }
 public class LinkedList{
      Node node;
      Node first;
-     public void IF(int data){
-          node = new Node();
+     public void InsertFirst(int data){
+          node= new Node();
           node.data=data;
           node.link=null;
           if(first==null){
-               first = node;
+               first=node;
           }
           else{
                node.link=first;
                first=node;
           }
      }
-     
-     public void display(){
-          Node temp=first;
+     public void DeleteFirst(){
           if(first==null){
-               System.out.println("NUll");
+               System.out.println("List Is Empty");
           }
           else{
+        Node del_data=first;
+                    System.out.println("Deleted data->"+del_data.data);
+                    del_data=del_data.link;
+          }
+     }
+     public void DisplayList(){
+          if(first==null){
+               System.out.println("List is Empty ");
+          }
+          else{
+               Node temp=first;
                while(temp!=null){
-                    System.out.println("Temp data"+temp.data);
+                    System.out.println("Data In the List->" +temp.data );
                     temp=temp.link;
                }
           }
      }
-     public void Delete(){
-          if(first==null){
-               System.out.println(" null");
-          }
-          else{
-               System.out.println("First data deleted "+first.data);
-               first=first.link;
-          }
-     }
-
      public static void main(String [] args){
-          Scanner sc =  new Scanner (System.in);
-          
+          Scanner sc= new Scanner(System.in);
+          LinkedList  ll= new LinkedList();
           int choice=0;
-          LinkedList ls= new LinkedList();
-          
-         do{
-              System .out.println("Enter the case 1 or 2 and 3 and 4");
-              choice = sc.nextInt();
-              switch(choice){
-              
-              case 1:
-                   System.out.println("ENter the data");
-                   int data=sc.nextInt();
-                   ls.IF(data);
-                   break;
-             case 2:
-               ls.Delete();
-               break;
-             case 3:
-               ls.display();
-               break;
+
+         do{ 
+              System.out.println("Enter the choice \n 1.Insert At first \n 2.Delete at first \n 3.Display List Elemnts ");   
+            choice = sc.nextInt();
+          switch(choice){
            
-          case 5:
-               System.out.println("Exiting..");
-               break;
-         
-         }
+            case 1: 
+                 System.out.println("Enter the data");
+                 int data=sc.nextInt();
+                 ll.InsertFirst(data);
+                 break;
+            case 2:
+                 ll.DeleteFirst();
+                 break;
+             case 3:
+                  ll.DisplayList();
+                  break;
+          }
          }while(choice<=5);
-}}
+     }
+}
