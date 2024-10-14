1a9c9fa516aa07d3d8d425c8f46add1f9d6020e6
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-10-14 21:33:50 +0530

    Update number is a prime.java

--- Basics java Foundations/number is a prime.java
+++ Basics java Foundations/number is a prime.java
@@ -1,26 +1,21 @@
 import java .util. Scanner ;
 
 public class Main{
-
-public static void main(String [] args)
-{
-Scanner sc = new Scanner (System.in);
-int num=sc.nextInt();
-
-for(int i=0;i<= num;i++){
-    num=sc.nextInt();
-    
-    int count=0;
-    for(int div=2;div * div<=num; div++){
-        if(num%div==0){
+        public static void main(String [] args){
+        Scanner sc = new Scanner (System.in);
+        int num=sc.nextInt();
+        for(int i=0;i<= num;i++){
+          int count=0;
+        for(int div=2;div * div<=num; div++){
+           if(num%div==0){
             count++;
             break;
         }
-    }
-        
+    } 
         if(count==0)
         System.out.println("PRIME NUMBER");
         else
           System.out.println(" NOT PRIME NUMBER");
-}}
+}
+}
 }
