94e629de0ecac0c5ec06b9549b87e2e44a9991fd
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-10-19 21:10:28 +0530

    Update MatricConversion.java

--- Basics java Foundations/MatricConversion.java
+++ Basics java Foundations/MatricConversion.java
@@ -4,9 +4,10 @@ public class MatricConversion{
     
        public static void main(String [] args){
          Scanner sc= new Scanner(System.in);
-              double farenheit=0,celcius=0;
-              System.out.println("Enter The Farenheit");
-              celcius=(((farenheit-32)*5)/9);
+          System.out.println("Enter The Farenheit");
+              double farenheit=sc.nextDouble();
+             
+             double celcius=(((farenheit-32)*5)/9);
               System.out.println("Converted Celcius "+celcius);
     }
 }
