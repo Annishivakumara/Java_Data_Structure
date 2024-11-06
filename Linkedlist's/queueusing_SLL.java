0dc62417fec51aa7b19912726dfc3b1b4ba52f6f
Anni <142305502+Annishivakumara@users.noreply.github.com>
2024-11-06 19:22:09 +0530

    Update queueusing_SLL.java

--- Linkedlist's/queueusing_SLL.java
+++ Linkedlist's/queueusing_SLL.java
@@ -51,7 +51,6 @@ public class QueueUsingSlist{
                }
           }
      }
-        
        public static void main(String [] args){
             Scanner sc= new Scanner(System.in);
             int choice;
