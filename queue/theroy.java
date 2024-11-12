13295ac29e6527943468c504cded9f13d160cf56
Annishivakumara <ashivu656@gmail.in>
2024-11-12 21:30:20 +0530

    new added

---
+++ queue/theroy.java
@@ -0,0 +1,15 @@
+/* 
+public class theroy{
+Queue
+
+• A stack is LIFO (Last-In First Out) structure. In
+contrast, a queue is a FIFO (First-In First-Out )
+structure.
+• In a FIFO data structure, the first element
+added to the queue will be the first one to be
+removed.
+• A queue is a linear structure for which items
+can be only inserted at one end and removed at
+another end.
+}
+*/
---
+++ stack/BalancingSymbol.java
@@ -0,0 +1,57 @@
+package stack;
+import java.util.*;
+
+public  class BalancingSymbol {
+	static int  top;
+	int size;
+	char [] ch;
+	public BalancingSymbol(int size) {
+		this.size=size;
+		ch= new char[size];
+		top=-1;
+	}
+	public void push(char symb) {
+		ch[++top]=symb;
+	}
+	public char pop() {
+		return ch[top--];
+	}
+	public char TopSymbol() {
+		return ch[top];
+	}
+	public boolean isEmpty() {
+		return top==-1;
+	}
+	public static void main(String [] args) {
+		Scanner sc= new Scanner(System.in);
+		char symb;
+	        System.out.println("Enter the Symbol Expression");
+                    String symbol=sc.nextLine();
+                    int length=symbol.length();
+                    BalancingSymbol bs= new  BalancingSymbol(length);
+               for(int i=0;i<symbol.length();i++){
+               symb=symbol.charAt(i);
+               
+               if(symb=='{'|| symb=='['|| symb=='('){
+                    bs.push(symb);
+               }
+               
+               else if(
+                    (symb=='}' &&  bs.TopSymbol()=='{')||
+                    (symb==']' && bs.TopSymbol()=='[' )||
+                    (symb==')' && bs.TopSymbol()=='(')){
+                    bs.pop();
+               }
+               
+               else if(symb=='}' || symb==')' || symb==']'){
+                    System.out.println("Symbol's Are not Balanced");
+                    return;
+	       }
+	       }
+                    if (bs.isEmpty()) {
+                    System.out.println("Symbols are Balanced");
+                          } else {
+                    System.out.println("Symbols are Not Balanced");
+                 }   
+
+          }}
