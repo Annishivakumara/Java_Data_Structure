2f17c4b53964414bd47c06baed30c20fd408d08f
Annishivakumara <ashivu656@gmail.in>
2024-11-16 23:04:55 +0530

    file Linked list updated

---
+++ Linkedlist/DoublyLinkedList.java
@@ -0,0 +1,185 @@
+package Linkedlist;
+import java.util.*;
+class Node{
+     int data;
+     Node leftLink;
+     Node rightLink;
+}
+  public class DoublyLinkedList{
+       Node node;
+       Node first , last;
+       public Node createNode(int data){
+            node=new Node();
+            node.data=data;
+            node.leftLink=null;
+            node.rightLink=null;
+           return node;
+       }
+       public void InsertFront(int data){
+           node= createNode(data);
+            if(first==null){
+                 first=last=node;
+            }else{
+                 node.rightLink=first;
+                 first.leftLink=node;
+                 first=node;
+            }
+       }
+       public void InertLast(int data){
+      node=createNode(data);
+            if(last==null){
+                first=last=node;
+            }else{
+                 node.leftLink=last;
+                 last.rightLink=node;
+                 last=node;
+            }
+       }
+       public void InsertAtSpecific(int data , int pos){
+           node = createNode(data);
+            if(first==null){
+                if(pos==1){
+                 first=last=node;
+                }else{
+               System.out.println("List is Empty");
+               }
+            }
+            else if(pos==1){
+               node.rightLink=first;
+               first.leftLink=node;
+               first=node;
+            }else{
+               Node temp=first;
+               int cnt=1;
+               while(temp!=null && cnt<pos-1){
+                  temp=temp.rightLink;
+                  cnt++;
+               }
+               if(temp==null){
+                    System.out.println("Inavalid List");
+               }
+              node.leftLink=temp;
+              node.rightLink=temp.rightLink;
+              if(temp.rightLink!=null)
+              temp.rightLink.leftLink=node;
+            else{
+                 last=node;
+            }
+              temp.rightLink=node;
+            }
+       }
+       public void DeleteAtSpecific(int pos){
+            if(first==null){
+               System.out.println("List is Null");
+            }else if(pos==1){
+               System.out.println("Deleteed At Position 1 "+first.data);
+               // Additional
+               first=first.rightLink;   
+               if(first!=null){//no nessary condition for understanding pourpose
+                    first.leftLink=null;
+               }else{
+                    last=null;
+               }
+            }
+            else{
+               Node temp=first;
+               int cnt=1;
+               while(temp!=null && cnt<pos){
+                 temp=temp.rightLink;
+                 cnt++;
+                 }
+                 if(temp==null){
+                      System.out.println("Invalid Position.. ");
+                 }
+                 System.out.println("Deleted data:  >>" +temp.data);
+                   if(temp.leftLink!=null){
+                        temp.leftLink.rightLink=temp.rightLink;
+                   }
+                  if(temp.rightLink!=null){
+                     temp.rightLink.leftLink=temp.leftLink;
+                  }
+                  if(temp==last){
+                     last=last.leftLink;
+                 }
+            }    
+             }
+           public void DeleteLast(){
+                if(last==null){
+                     System.out.println("List is Null");
+                }else{
+                    Node temp=last;
+                    System.out.println("Deleted Data >>"+last.data);
+                    temp=temp.leftLink;
+                    if(temp!=null){
+                    temp.rightLink=null;
+                    }else{
+                    first=null;
+                    }
+                }
+           }
+           public void DeleteFront(){
+                if(first==null){
+                     System.out.println("Doubly Linked List is Nulll");
+                }else{
+                       Node temp=first;
+                       System.out.println("Deleted Data At Specific >"+temp.data);
+                      temp= temp.rightLink;
+                      if(temp!=null){
+                       temp.leftLink=null;
+                      }else{
+                           last=null;
+                      }
+                }
+           }
+           public void Display(){
+                if(first==null){
+                     System.out.println("Doubly List is Empty");
+                }else{
+                  Node temp=first;
+                  while(temp!=null){
+                       System.out.println("Data in the List >>"+temp.data);
+                       temp=temp.rightLink;
+                  }
+                }
+           }
+       public static void main (String[] args) {
+        Scanner sc= new Scanner(System.in);
+        DoublyLinkedList dl= new DoublyLinkedList();
+        int choice=1;
+        do{
+             System.out.println("Entee the Choice \n 1.InsertFront \n 2.InertLast \n 3.InsertAtSpecific \n 4.DeleteAtSpecific \n 5.DeleteLast \n 6.DeleteFront   \n 7.Display");
+             choice=sc.nextInt();
+             switch(choice){
+               case 1:
+                        System.out.println("Enter the  Inserting Front Data");
+                        int data=sc.nextInt();
+                        dl.InsertFront(data);
+                        break;
+               case 2:
+                        System.out.println("Enter the Inserting Last Data");
+                        int data1=sc.nextInt();
+                        dl.InertLast(data1);
+                        break;
+               case 3:
+                        System.out.println("Enter the InsertingAt Specific Last  1.Data \n 2.Position");
+                        int data3=sc.nextInt();
+                        int pos= sc.nextInt(); 
+                        dl.InsertAtSpecific(data3, pos);
+                        break;
+               case 4:
+                        int pos1=sc.nextInt();
+                        dl.DeleteAtSpecific(pos1);
+                        break;
+               case 5:
+                        dl.DeleteLast();
+                        break;
+               case 6: 
+                         dl.DeleteFront();
+                         break;
+               case 7:
+                        dl.Display();
+                        break;
+             }
+        }while(choice<=15);
+       }
+  }
---
+++ Linkedlist/LinkedList.java
@@ -0,0 +1,193 @@
+package Linkedlist;
+import java.util.*;
+ class Node{
+      int data;
+      Node link;
+ }
+ public class LinkedList{
+      Node node;
+      Node first;
+      public Node createNode(int data){
+           node=new Node();
+           node.data=data;
+           node.link=null;
+          return node;
+      }
+      public void insertFront(int data){
+           node=createNode(data);
+           if(first==null){
+                first=node;
+           }
+           else{
+                node.link=first;
+                first=node;
+           }
+      }
+      public void InsertEnd(int data){
+           node= createNode(data);
+           if(first==null){
+                System.out.println("List is Empty");
+           }
+           else{
+                Node temp= first;
+                while(temp.link!=null){
+                     temp=temp.link;
+                }
+                temp.link=node;
+           }
+      }
+      public void InsertSpecific( int pos, int data){
+           node= createNode(data);
+           if(pos<1){
+                System.out.println("Enter Valid Position");
+           }
+           else if(pos==1){
+                 node.link=first;
+                 first=node;
+           }
+           else{
+               Node current=first;
+               int count=1;
+               while(current !=null && count<pos-1){
+                    current=current.link;
+                    count++;
+               }
+               if(current==null){
+                    System.out.println("Postion wrong");
+               }else{
+               node.link=current.link;
+               current.link=node;}
+           }
+      }
+      public void DeleteFront(){
+           if(first==null){
+                System.out.println("List is Empty");
+           }
+           else{
+                System.out.println("Deleted data   > "  +first.data );
+               first=first.link;
+           }
+      }
+      public void DeleteLast(){
+         if(first==null){
+             System.out.println("Null list");
+         }
+         if(first.link==null){
+                System.out.println("Deleted at last" +first.data);
+             first=null;
+         }
+         else{
+             Node temp=first;
+             while(temp.link.link!=null){
+                 temp=temp.link;
+             }
+                 System.out.println("Deleted at last" +temp.link.data);
+                  temp.link=null;
+         }
+     }
+     public void DeletedAtSpecific(int pos){
+          if(first==null){
+               if(pos<1){
+               System.out.println("Enter the Valid posiion");
+               System.out.println("List is Null");
+          }
+          }
+          else if(pos==1){
+               System.out.println("Deleted data > "+first.data);
+               first=first.link;
+          }
+          else{
+               Node Current=first;
+               int count=1;
+               while(Current!=null && count< pos-1){
+                    Current=Current.link;
+                    count++;
+               }
+               if(Current ==null ||  Current.link==null){
+                    System.out.println("Enter the valid postion");
+               }
+               else{
+                    System.out.println("deleted pos"+pos+ " " +Current.link.data);
+                    Current.link=Current.link.link;
+               }
+          }
+     }
+  public void search(int data){
+  Node temp=first;
+   if(temp==null){
+    System.out.println("LIST IS NULL");
+   }else{
+    while(temp!=null){
+     if(temp.data==data){
+      System.out.println("Data Found");
+   }
+     break;
+    }
+    if(temp==null){
+     System.out.println("Data not Found");
+    }
+   }
+  }
+     public void Display(){
+          if(first==null){
+               System.out.println("List is Null");
+          }
+          else{
+               Node temp=first;
+               while(temp!=null){
+                    System.out.println("data in list > "+temp.data);
+                    temp=temp.link;
+               }
+          }
+     }
+      public static void main(String [] args){
+       Scanner sc= new Scanner(System.in);
+       int choice=0;
+       LinkedList ll= new LinkedList();
+       
+       do{
+    System.out.println("Enter the choice \n1.Insert Front \n2.Delete Front\n3.Display \n4.InsertAt end\n5.delete End\n6.Insert At specific \n7.Delete at Specific \n8.Search the Element");
+            choice=sc.nextInt();
+            switch(choice){
+            case 1:
+                 System.out.println("Enter the Data");
+                 int data=sc.nextInt();
+                 ll.insertFront(data);
+                 break;
+           case 2:
+                ll.DeleteFront();
+                break;
+           case 3:
+                ll.Display();
+                break;
+          case 4:
+               System.out.println("Ente the data");
+               int data1=sc.nextInt();
+               ll.InsertEnd(data1);
+               break;
+          case 5:
+               ll.DeleteLast();
+               break;
+          case 6:
+               System.out.println("Enter the \n 1.Position \n 2.Data");
+               int pos=sc.nextInt();
+               int data2 =sc.nextInt();
+               ll.InsertSpecific(pos,data2);
+               break;
+          case 7:
+               System.out.println("Ente the \n 1.Position ");
+               int pos2=sc.nextInt();
+               ll.DeletedAtSpecific(pos2);
+               break;
+         case 8:
+              System.out.println("Searching Data:  Enter the Data ");
+              int data3= sc.nextInt();
+              ll.search(data3);
+              break;
+          default:
+          System.out.println("Enter the valid Choice");
+       }  
+       }while(choice<=9);
+       
+      }
+ }
