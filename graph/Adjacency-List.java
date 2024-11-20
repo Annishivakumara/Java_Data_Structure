4bf8e1e8539e54e37b1fa089ab09b117486ebb4b
Anni <142305502+Annishivakumara@users.noreply.github.com>
2024-11-21 04:10:57 +0530

    Update Adjacency-List.java

--- graph/Adjacency-List.java
+++ graph/Adjacency-List.java
@@ -1,7 +1,7 @@
 package graph;
 import java.util.Scanner;
 import  java.util.ArrayList;
-public class impli{
+public class Adjacence_List{
 class Edge{
     int src;
     int dest;
@@ -10,16 +10,32 @@ class Edge{
         this.dest=dest;
     }
 }
-public void creategraph(ArrayList<Edge> graph[]){ 
+//CreateGraph Method 
+public  static  void  creategraph(ArrayList<Edge> graph[]){ 
    for(int i=0;i<graph.length;i++){
     graph[i]=new ArrayList<Edge>();
    }
+   graph[0].add(new Edge(0, 2));
+   graph[1].add(new Edge(1, 2));
+   graph[1].add(new Edge(1, 3));
+   graph[2].add(new Edge(2, 0));
+   graph[2].add(new Edge(2, 1));
+   graph[2].add(new Edge(2, 3));
+   graph[3].add(new Edge(3, 1));
+   graph[3].add(new Edge(3, 2));
 }
     public static void main(String [] args){
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter the  Number of Vertexe's ");
         int v=sc.nextInt();
    
-       ArrayList<Edge> graph[]= new ArrayList[v];
+        ArrayList<Edge>  [] graph= new ArrayList[v];
+        creategraph(graph);
+       
+        // Print The graph
+        for(int i=0;i<graph[2].size();i++){
+            Edge e=graph[2].get(i);
+            System.out.println(e.src);
+        }
     }
 }
