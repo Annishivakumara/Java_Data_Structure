d160714cb7b3278906c628ac0b06814dc5d38988
Anni <142305502+Annishivakumara@users.noreply.github.com>
2024-12-04 01:01:38 +0530

    Update insertion.java

--- sorting/insertion.java
+++ sorting/insertion.java
@@ -1,33 +1,45 @@
-
 import java.util.*;
-public class Main{
-    int [] arr=new int[7];
+
+public class insertion{
+    int size;
+    int [] arr;
+    public insertion(int size){
+        this.size=size;
+       arr= new int[size];
+    }
+    public void read(){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the Number: ");
+       for(int i=0;i<arr.length;i++){
+        arr[i]=sc.nextInt();
+       }
+        }
     
-    public void read() {
-        Scanner sc = new Scanner(System.in);  
-        System.out.println("Enter the numbers:");
-        for (int i = 0; i < arr.length; i++) { 
-            arr[i] = sc.nextInt();
-        }
-    }
     public void insert(){
-        for( int i=1;i<arr.length;i++){
-            int key = arr[i];
-            int j=i-1;
-            while(j>=0 && arr[j]>key){
-                if(arr[j]>key){
-                    arr[j+1]=arr[j];
-                    j--;
-                }
-                arr[j]=key;
+        for(int i=1;i<arr.length;i++){
+           int data=arr[i];
+           int j=i-1;
+           while(j>=0 && arr[j]>data){
+           if(arr[j]>data){
+                arr[j+1]=arr[j];
+                j--;
             }
+            arr[j+1]=data;
+          }
         }
+        System.out.println("Sorted Array");
+        for(int s:arr){
+            System.out.print(s+" ");
+        }
+        System.out.println();
     }
-    public static void main(String[] argfs){
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        Main m= new Main();
-        m.read();
-        m.insert();
-    }
+
+        public static void main(String[] args) {
+            Scanner sc= new Scanner(System.in);
+            System.out.println("Enter The Size Of the Arrsy: ");
+            int size=sc.nextInt();
+            insertion is= new insertion(size);
+            is.read();
+            is.insert();
+        }
 }
