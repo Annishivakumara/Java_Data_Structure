01cb46cd2d0a9354ff99ed0d68e2d45ee45e016c
Anni <142305502+Annishivakumara@users.noreply.github.com>
2024-12-03 23:46:51 +0530

    Update selection.java

--- sorting/selection.java
+++ sorting/selection.java
@@ -1,1 +1,39 @@
 
+package  sorting;
+import java.util.*;
+
+public class  selection {
+    int arr[] = new int[5];
+
+    public void read() {
+        Scanner sc = new Scanner(System.in);  
+        System.out.println("Enter the numbers:");
+        for (int i = 0; i < arr.length; i++) { 
+            arr[i] = sc.nextInt();
+        }
+    }
+
+   public void select(){
+    int min;
+    for(int i=0;i<=arr.length;i++){
+        min=i;
+        for(int j=i+1;j<=arr.length;j++){
+            if(arr[j]<arr[min]){
+                arr[min]=arr[j];
+            }
+        }
+        int temp=arr[min];
+        arr[min]=arr[i];
+        arr[i]=temp;
+    }
+   }
+
+    public static void main(String[] args) {
+        selection ls = new selection();
+        ls.read();  
+        Scanner sc = new Scanner(System.in);  
+        //  System.out.println("Enter data to search:");
+        // int data = sc.nextInt();
+        ls.select();  
+    }
+}
