71cb300492d9c8ed8db11eb4412f5c735b9ef556
Anni <142305502+Annishivakumara@users.noreply.github.com>
2024-12-09 00:20:36 +0530

    Update margesort.java

--- sorting/margesort.java
+++ sorting/margesort.java
@@ -1,67 +1,64 @@
-
 import java.util.*;
-
-class Solution {
-    private static void merge(int[] arr, int low, int mid, int high) {
-        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
-        int left = low;      // starting index of left half of arr
-        int right = mid + 1;   // starting index of right half of arr
-
-        //storing elements in the temporary array in a sorted manner//
-
-        while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right]) {
-                temp.add(arr[left]);
-                left++;
-            } else {
-                temp.add(arr[right]);
-                right++;
-            }
+class sort{
+    public static void margesort1(int arr[] , int low, int high){
+        if(low>=high){
+            return ;
         }
-
-        // if elements on the left half are still left //
-
-        while (left <= mid) {
+        int mid=(low+high)/2;
+        margesort1(arr,low, mid);
+        margesort1(arr,mid+1,high);
+        marge(arr, low,mid, high);
+    }
+    public static void marge(int arr[], int low, int mid , int high){
+    ArrayList <Integer>  temp= new ArrayList<>();
+    int left=low;
+    int right=mid+1;
+    while(left<=mid && right<=high ){
+        if(arr[left]<=arr[right]){
             temp.add(arr[left]);
             left++;
-        }
-
-        //  if elements on the right half are still left //
-        while (right <= high) {
+        }else{
             temp.add(arr[right]);
             right++;
         }
-
-        // transfering all elements from temporary to arr //
-        for (int i = low; i <= high; i++) {
-            arr[i] = temp.get(i - low);
-        }
     }
-
-    public static void mergeSort(int[] arr, int low, int high) {
-        if (low >= high) return;
-        int mid = (low + high) / 2 ;
-        mergeSort(arr, low, mid);  // left half
-        mergeSort(arr, mid + 1, high); // right half
-        merge(arr, low, mid, high);  // merging sorted halves
+    while(left<=mid){
+        temp.add(arr[left]);
+        left++;
     }
+    while(right<=high){
+        temp.add(arr[right]);
+        right++;
+    }
+    for(int i=low;i<=high;i++){
+        arr[i]=temp.get(i-low);
+    }
+} 
 }
-public class tUf {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = 7;
-        int arr[] = { 9, 4, 7, 6, 3, 1, 5 };
-        System.out.println("Before sorting array: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-        Solution.mergeSort(arr, 0, n - 1);
-        System.out.println("After sorting array: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
+public class margesort {
+ static    int [] arr;
+    public  margesort(int size){
+        arr= new int[size];
     }
-
+    public  void read(){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter The Array Elements");
+    for(int i=0;i<arr.length;i++){
+       arr[i]=sc.nextInt();
+    }
+    System.out.println();
+    }
+    public static void main(String [] args){
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter The Array Size: ");
+    int size= sc.nextInt();
+    margesort mg= new margesort(size);
+    mg.read();
+    sort.margesort1(arr, 0, size-1);
+    for (int i = 0; i <size; i++) {
+        System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+    sc.close();
+    }
 }
